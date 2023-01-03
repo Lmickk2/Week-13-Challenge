a4d2c3c9ecc71389e818fccfb5c8e6ca3483e768
Lmickk2 <lukemickan21@icloud.com>
2023-01-03 13:36:55 -0500

    added final models, fixed category route. Tested post requests

--- 02-Challenge/Develop/models/ProductTag.js
+++ 02-Challenge/Develop/models/ProductTag.js
@@ -6,8 +6,32 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "tag",
+        key: "id"
+      }
+    },
+
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "product",
+        key: "id"
+      }
+    }
   },
+
   {
     sequelize,
     timestamps: false,
--- 02-Challenge/Develop/models/Tag.js
+++ 02-Challenge/Develop/models/Tag.js
@@ -1,13 +1,23 @@
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../config/connection.js');
+const sequelize = require('../config/connection');
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
+
   {
     sequelize,
     timestamps: false,
