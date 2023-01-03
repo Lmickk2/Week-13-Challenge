46922e25b5b080bb90e02c57005833f742382817
Lmickk2 <lukemickan21@icloud.com>
2023-01-02 19:50:14 -0500

    defined columns in Product and Category models

--- 02-Challenge/Develop/server.js
+++ 02-Challenge/Develop/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-const mysql = require('mysql')
-// import sequelize connection
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,7 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
